607f9f9ac451d6b0ecbbf6ef5f5296aa0d469914
Wellington Araujo <welitoaraujo7@gmail.com>
2026-02-12 14:24:29 -0400

    feat: generate prisma client

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import prisma from './shared/database/prisma';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (_, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
 });
 
 export default app;
